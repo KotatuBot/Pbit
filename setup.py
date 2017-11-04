f55e022fbf66bb08e45e87280c27520dc4758d9e
KotatuBot <kotatubot@gmail.com>
2017-11-04 14:11:08 +0900

    test setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+        name="Pbit",
+        version="0.1",
+        description="This library that helps to manipulate the numbers in Python",
+        url="https://github.com/KotatuBot/Pbit",
+        packages=find_packages(),
+        entry_points="""
+        """
+        )
